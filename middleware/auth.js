c557828c2c2a2cc050100f5412883e8bdeb0b84a
Daianne Aguiar <daiannegaguiar@gmail.com>
2022-06-21 20:14:43 -0300

    created const to authentication and DB

---
+++ middleware/auth.js
@@ -0,0 +1,21 @@
+const { status } = require("express/lib/response")
+const jwt = require("jsonwebbtoken")
+const cfg = require("../config/config")
+
+const auth = (req, res, next) => {
+    const token_created = req.headers.token
+
+    if(!token_created){
+        return res.status(401).send({output: 'Access denied'})
+    }
+    jwt.verify(token_created, cfg.jwt_key, (error,data)=> {
+        req.content = {
+            id:data.id,
+            user: data.user
+        }
+        return next();
+    });
+    
+}
+
+module.exports = auth;
---
+++ utils/token.js
@@ -0,0 +1,8 @@
+const jwt = require("jsonwebbtoken")
+const cfg = require("../config/config")
+
+const create_token = (id, user) => {
+    return jwt.sign({id:id, user:user},cfg.jwt_key,{expireIn:cfg.jwr_expires})
+}
+
+module.exports = create_token;
